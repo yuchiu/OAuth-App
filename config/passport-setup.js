b32d068e14a4a19e97b6460a923b250e2ae79154
yu <yuchiu2002@hotmail.com>
2018-01-04 21:59:39 -0500

    retrieving user

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,22 +1,33 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
-const keys= require('./keys')
+const keys = require('./keys')
 const User = require('../models/User')
 
 passport.use(
     new GoogleStrategy({
         //options for google strategy
         callbackURL: '/auth/google/redirect',
-        clientID:keys.google.clientID,
-        clientSecret:keys.google.clientSecret
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
     }, (accessToken, refreshToken, profile, done) => {
         //passport callback function
-        new User({
-            username: profile.displayName,
+
+        //check if user exist
+        User.findOne({
             googleId: profile.id
-        }).save().then((newUser)=>{
-            console.log('new user created: '+newUser)
+        }).then((currentUser) => {
+            if (currentUser) {
+                //the user is already registered
+                console.log('user already register, user is: ' + currentUser)
+            } else {
+                //create user in our db
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id
+                }).save().then((newUser) => {
+                    console.log('new user created: ' + newUser)
+                })
+            }
         })
     })
-
 )
