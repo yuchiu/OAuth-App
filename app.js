7efe2f3f794bea647a8c5412543dfbd57968e784
yu <yuchiu2002@hotmail.com>
2018-01-04 21:14:08 -0500

    setup route for google auth to redirect

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express =  require('express')
 const authRoutes = require('./routes/auth-routes')
+const passportSetup = require('./config/passport-setup')
 
 const app = express()
 
+//set up view engine
 app.set('view engine', 'ejs')
 
 //create route
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,17 +2,27 @@ const router = require('express').Router()
 const passport = require('passport')
 
 //auth login
-router.get('/login', (req, res, err) => {
+router.get('/login', (req, res) => {
     res.send('login')
 })
 
 //lgout
-router.get('/logout', (req, res, err) => {
+router.get('/logout', (req, res) => {
     //handle with passport
     res.send('logout')
 })
 
 //auth with google
-router.get('/google', passport.authenticate('google'))
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile']
+}))
+//callback route for google to redirect to
+router.get('/google/redirect', (req, res) => {
+    //handle with passport
+    res.send({
+        confirmation: "success",
+        message: "you reached the call back uri"
+    })
+})
 
 module.exports = router
