ce9d228f6ed0dd3d1d11194376c7dd3f9da6331d
yu <yuchiu2002@hotmail.com>
2018-01-09 21:34:52 -0500

    added middleware authCheck to redirect user if the user did not log in

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,5 +1,15 @@
 const router = require('express').Router()
-router.get('/', (req, res) => {
+
+const authCheck = (req, res, next)=>{
+    if(!req.user){
+        res.redirect('/auth/login')
+    }else{
+        next()
+    }
+}
+
+router.get('/', authCheck,(req, res) => {
     res.send('you are logged in, this is profile page, welcome '+ req.user.username)
 })
+
 module.exports = router
