30820d0b81edcdd649b962725e5f49603377d130
yu <yuchiu2002@hotmail.com>
2018-01-09 21:39:30 -0500

    added profile view page

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,15 +1,17 @@
 const router = require('express').Router()
 
-const authCheck = (req, res, next)=>{
-    if(!req.user){
+const authCheck = (req, res, next) => {
+    if (!req.user) {
         res.redirect('/auth/login')
-    }else{
+    } else {
         next()
     }
 }
 
-router.get('/', authCheck,(req, res) => {
-    res.send('you are logged in, this is profile page, welcome '+ req.user.username)
+router.get('/', authCheck, (req, res) => {
+    res.render('profile', {
+        user: req.user
+    })
 })
 
 module.exports = router
